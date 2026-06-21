592edef5c12489b2c227b00bcaa134b7bc889a47
Farhan-Akhtar3682 <m.farhanakhtar04@gmail.com>
2026-06-21 13:12:16 +0500

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000/api/todos";
+const API = "http://backend:5000/api/todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
